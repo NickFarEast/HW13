0f24a7b456e51cf3ebaf6241265bfe393021d72f
NickFarEast <shramko.dt@gmail.com>
2022-02-06 18:36:11 +1000

    завершен шаг 4

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request, render_template, send_from_directory
+from flask import Flask, request, render_template, send_from_directory,redirect
 from functions import get_post_by_tag, get_all_tags_from_posts
 
 
 UPLOAD_FOLDER = "uploads/images"
 
 app = Flask(__name__)
-
+app.config['JSON_AS_ASCII'] = False
 
 @app.route("/")
 def page_index():
@@ -23,12 +23,29 @@ def page_tag():
 
 @app.route("/post", methods=["GET", "POST"])
 def page_post_create():
-    pass
+    if request.method == "GET":
+        return render_template("post_form.html")
+    if request.method == "POST":
+        picture = request.files.get("picture")
+        content = request.values.get("content")
+
+        if not picture:
+            return redirect("/post")
+
+        filename = picture.filename
+        path = "./"+ UPLOAD_FOLDER+"/"+filename
+        picture.save(path)
+
+        picture_new_url = "/"+ UPLOAD_FOLDER+"/"+filename
+
+        return render_template("post_uploaded.html", picture=picture_new_url, content=content)
+
 
 
 @app.route("/uploads/<path:path>")
 def static_dir(path):
     return send_from_directory("uploads", path)
+
 
 
 app.run()
